9d7ad263e03b3f8cbe5da8e1aa5d319e00c21b39
Sadman Hossain <m.sadman.h@gmail.com>
2021-08-06 16:40:59 +0000

    Add chatroom function

--- src/App.js
+++ src/App.js
@@ -55,7 +55,21 @@ function SignOut() {
 }
 
 function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, {idField: 'id'});
 
+  return (
+    <div>
+      {messages && messages.map(msg => <ChatMessage key = {msg.id} message={msg} />)}
+    </div>
+  )
+}
+
+function ChatMessage(props) {
+  const {text, uid } = props.message;
+
+  return <p>{text}</p>
 }
 
 export default App;
